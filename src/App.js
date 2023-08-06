865350f245ad5a4ab293c32445474b3985de3ba4
Vishal Shrivastava <vishal71shrivastava@gmail.com>
2023-08-06 19:09:22 +0530

    first react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import Header from "./component/Header";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+  <h1>hello react</h1>
+  <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, fugiat iusto ipsa quaerat rem quo. Eius veritatis magni harum dolore minus eaque ab sed quo! Expedita dolor eligendi maxime voluptates dolores fuga explicabo quas, nemo, laudantium sint cupiditate adipisci, id tempore exercitationem facere consequatur est dignissimos debitis doloribus. Dolores assumenda consequuntur id aliquam earum quis cum, nobis ex itaque voluptates, expedita tenetur! Facere aut itaque dolore excepturi maiores ipsam aspernatur dignissimos deleniti ratione! Perspiciatis, neque facere quis voluptatum ducimus soluta aperiam at, eos similique nostrum earum temporibus! Laudantium natus quasi alias, at eaque numquam similique temporibus sunt porro! Quasi, ratione.</h5>
+  <h1>
+    changes is comes on react
+  </h1>
+ <h6>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab maxime totam eaque id similique mollitia dolore accusantium tenetur animi! Consequatur, commodi? Quod, in. Unde officia adipisci eligendi tempore excepturi ipsa.</h6>
+    </>
+  );
+}
+
+export default App;
